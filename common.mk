f461c7f7f2804104a44bae8e507b83f8a7104943
DigiGoon <kagatharatarang@gmail.com>
2021-02-23 18:40:19 +0530

    sm8150-common: Drop Snap & build GCamGO *Until a stable OP cam or GCam doesn't land
    Signed-off-by: DigiGoon <kagatharatarang@gmail.com>

--- common.mk
+++ common.mk
@@ -158,7 +158,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    Snap \
+    GCamGOPrebuilt \
     vendor.oneplus.camera.CameraHIDL@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
