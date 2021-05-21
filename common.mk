87e92c447238b8c3b8380f793e28c72861e6d380
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-21 09:15:27 +0000

    sm8150-common: remove unnecessary trailing \
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I8baa62d934efbcf28e4a7329cc969dc1c7479e79

--- common.mk
+++ common.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
     libspkrprot \
     libtinycompress \
     libvolumelistener \
-    tinymix \
+    tinymix
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
@@ -458,7 +458,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
+    android.hardware.usb@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
