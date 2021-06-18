7c951663e801b6bfbbf7efae6d5c5c767afa7d68
Pranav Vashi <neobuddy89@gmail.com>
2021-06-19 03:31:46 +0530

    sm8150-common: Compile missing libcamera2ndk_vendor
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -158,6 +158,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     libxml2 \
     GCamGOPrebuilt \
     vendor.oneplus.camera.CameraHIDL@1.0.vendor \
