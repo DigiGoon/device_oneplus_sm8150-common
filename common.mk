ef2820ef9d6dfb16ce6fa6f2aa35157c0098b684
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-10-27 12:47:02 +0530

    sm8150-common: rootdir: uprev to LA.UM.9.1.r1-11200.01-SMxxx0.QSSI12.0
    * mildy remove/beautify crap
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -151,12 +151,8 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
-    ftm_power_config.sh \
     init.class_main.sh \
-    init.cust.rc \
-    init.oem.debug.rc \
     init.oem.rc \
-    init.oem_ftm.rc \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.factory.rc \
@@ -169,9 +165,7 @@ PRODUCT_PACKAGES += \
     init.qti.qcv.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    vendor.oem_ftm.rc \
-    vendor.oem_ftm_svc_disable.rc
+    ueventd.qcom.rc
 
 # Display
 PRODUCT_PACKAGES += \
