483527db7356567428117a4799417099bdcd84fc
timocapa <timo.capa@gmail.com>
2022-05-07 00:30:30 +0200

    sm8150-common: Enable Wifi Aware
     * A year ago this didn't work at all and caused major log spam
       Log spam seems to be gone and not affect anything else, however
       I can't verify whether it actually works.

--- common.mk
+++ common.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     config.disable_rtt=true
