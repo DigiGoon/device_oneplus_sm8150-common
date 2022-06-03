3b2124f1c32cb130f161979154e1ce29865d1ec3
timocapa <timo.capa@gmail.com>
2022-06-03 23:24:31 +0200

    common: Enable semi-aggressive VRR
     * I may tweak this at a later point, or split it up across devices,
       depending on user feedback
    
     * Below around 15% force 90 hz
    
     * To get the brightness in integer from the UI percentage, use
       "settings get system screen_brightness"

--- common.mk
+++ common.mk
@@ -105,6 +105,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.fuse.passthrough.enable=true
 
+# Automatic refresh rate switching for power savings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms=800 \
+    ro.surface_flinger.set_touch_timer_ms=800 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
