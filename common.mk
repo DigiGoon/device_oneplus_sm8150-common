194933699f9a92a77539dd7c06d53500663d09ad
timocapa <timo.capa@gmail.com>
2022-06-16 23:00:30 +0200

    common: make VRR less aggressive

--- common.mk
+++ common.mk
@@ -107,9 +107,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Automatic refresh rate switching for power savings
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=800 \
-    ro.surface_flinger.set_touch_timer_ms=800 \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=1500 \
+    ro.surface_flinger.set_touch_timer_ms=1500 \
+    ro.surface_flinger.set_display_power_timer_ms=1500 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 # Bluetooth
